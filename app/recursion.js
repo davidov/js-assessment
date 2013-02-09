27d61ecb5c6dce3565c0084d436cd6b60ce8aca9
Dror Avidov <just2click@outlook.com>
2013-02-09 21:47:11 +0200

    Only one failure left

--- app/recursion.js
+++ app/recursion.js
@@ -61,7 +61,24 @@ define(function() {
     },
 
     permute: function(arr) {
+        var permArr = [], usedChars = [];
+        
+        function permuteArray(input) {
+           var i, ch;
+            for (i = 0; i < input.length; i++) {
+                ch = input.splice(i, 1)[0];
+                usedChars.push(ch);
+                if (input.length === 0) {
+                    permArr.push(usedChars.slice());
+                }
+                permuteArray(input);
+                input.splice(i, 0, ch);
+                usedChars.pop();
+            }
+            return permArr;
+        }
 
+        return permuteArray(arr);
     }
   };
 });
